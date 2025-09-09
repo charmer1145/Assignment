eb9e99546bd790fe7545408d10b973cbd1254e70
charmer1145 <manishvermaa45@gmail.com>
2025-09-09 22:25:25 +0530

    simple calculator

---
+++ calculator.java
@@ -0,0 +1,31 @@
+public class Calculator {
+    public static void main(String[] args) {
+        if (args.length != 3) {
+            System.out.println("Usage: java Calculator <num1> <operator> <num2>");
+            return;
+        }
+
+        double num1 = Double.parseDouble(args[0]);
+        String operator = args[1];
+        double num2 = Double.parseDouble(args[2]);
+        double result = 0;
+
+        switch (operator) {
+            case "+": result = num1 + num2; break;
+            case "-": result = num1 - num2; break;
+            case "*": result = num1 * num2; break;
+            case "/": 
+                if (num2 != 0) result = num1 / num2;
+                else {
+                    System.out.println("Error: Division by zero");
+                    return;
+                }
+                break;
+            default: 
+                System.out.println("Invalid operator. Use +, -, *, /");
+                return;
+        }
+
+        System.out.println("Result: " + result);
+    }
+}
