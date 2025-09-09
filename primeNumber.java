d66bede5b9be6e20ca72833c1ff58019eae46b82
charmer1145 <manishvermaa45@gmail.com>
2025-09-09 22:39:07 +0530

    check prime number

--- primeNumber.java
+++ primeNumber.java
@@ -19,7 +19,7 @@ public class PrimeCheck {
         }
 
         if (isPrime)
-            System.out.println(num + " is a prime number.");
+            System.out.println(num + " is a prime number");
         else
             System.out.println(num + " is not a prime number.");
     }
